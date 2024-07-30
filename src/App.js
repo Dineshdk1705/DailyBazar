c0c1606ee78ff9a5f67bc3f6f04cb62cbccdbaaf
Dineshdk1705 <dnsh555g@gmail.com>
2024-07-30 16:41:15 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="bg-gray-50">
-      <div className="px-5 md:px-36 py-4 w-screen sticky top-0 bg-gray-50">
+      <div className="px-5 md:px-36 w-screen sticky top-0 bg-gray-50">
         <Header />
       </div>
       <Outlet />
